d17d9d5380b9d4db5fa5cef060489bcbc4b90a70
Chris McDonald <mail.chris.mcdonald@gmail.com>
2015-12-14 13:11:09 -0700

    solution to 14

---
+++ 14/14.py
@@ -0,0 +1,34 @@
+from itertools import accumulate
+
+def reindeer_race(input_list, length):
+    while len(input_list) < length:
+        input_list += input_list
+    
+    return input_list[:length]
+
+
+lookup = {}
+
+with open("input.txt", "r") as input:
+    for line in input:
+        words = line.split()
+        reindeer = words[0]
+        speed = int(words[3])
+        duration = int(words[6])
+        rest = int(words[-2])
+        lookup[reindeer] = [speed] * duration + [0] * rest
+
+
+print(max(sum(reindeer_race(race, 2503)) for race in lookup.values()))
+
+running_totals = [list(accumulate(reindeer_race(race, 2503))) for race in lookup.values()]
+running_totals = list(zip(*running_totals))
+points = [0] * len(list(lookup.keys()))
+
+
+for position_tuple in running_totals:
+    for position in range(len(points)):
+        if position_tuple[position] == max(position_tuple):
+            points[position] += 1
+
+print(max(points))
