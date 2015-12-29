5621cf69741ad421a45fd30618fe70e5b64c1b51
Chris McDonald <mail.chris.mcdonald@gmail.com>
2015-12-28 21:54:09 -0700

    cleanups on 25

--- 25/25.py
+++ 25/25.py
@@ -1,16 +1,14 @@
-
 def cantor_pair(x, y):
     x -= 1
     y -= 1
     return (((x + y) * (x + y + 1)) // 2) + y + 1
-    
+
 
 def nth_code(n):   
     return (20151125 * pow(252533, n-1, 33554393)) % 33554393
-    
+
 
 row = 2978
 col = 3083
 
 print(nth_code(cantor_pair(row, col)))
-
